999c9e84a8a11cabec5736500c26b738f9a1b492
root <root@ip-172-31-41-7.ap-northeast-1.compute.internal>
2020-05-07 17:04:24 +0000

    removed gtag manager

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,19 +3,6 @@
     <head>
         <!-- Anti-flicker snippet (recommended)  -->
         <style>.async-hide { opacity: 0 !important} </style>
-        <script>(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
-        h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
-        (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
-        })(window,document.documentElement,'async-hide','dataLayer',4000,
-        {'GTM-WX4D4SN':true});</script>
-        <!-- Google Tag Manager -->
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-WX4D4SN');</script>
-        <!-- End Google Tag Manager -->
-
 
         <title>Minimum Network</title>
         <meta name="csrf-token" content="{{ csrf_token() }}" charset="UTF-8">
@@ -26,14 +13,7 @@
         <link rel="stylesheet" href="//fonts.googleapis.com/icon?family=Material+Icons">
         <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css" type="text/css" >
         <link rel="stylesheet" href="{{ env('APP_URL').elixir('css/app.css') }}">
-        
     <body>
-        <!-- Google Tag Manager (noscript) -->
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WX4D4SN"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        <!-- End Google Tag Manager (noscript) -->
-
-        
         <div class="container">
             <div id="app" v-cloak></div>
         </div>
